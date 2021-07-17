f8e37af4e96c14fa0fe92d54bb746a4184014f12
speedracer05 <speedracer-05@outlook.com>
2021-07-17 08:04:32 -0700

    update index file

--- static/js/config.js
+++ static/js/config.js
@@ -1,3 +1,3 @@
 # API Key
 
-mapbox_key = "pk.eyJ1Ijoic3BlZWRyYWNlci0wNSIsImEiOiJja3F5M2Vkem4wejdkMm5uejRsODljcXE3In0.3LAhVB0jZSBmtJFDNOEg0A"
+mapbox_key = "pk.eyJ1Ijoic3BlZWRyYWNlci0wNSIsImEiOiJja3F5M2Vkem4wejdkMm5uejRsODljcXE3In0.3LAhVB0jZSBmtJFDNOEg0A";
