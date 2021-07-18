0d053f3016c4f705361b359bc20ef5b73114af99
Matthew Jensen <mjensen@cokergroup.com>
2021-07-18 14:42:07 -0700

    Update logic4

--- static/js/logic4.js
+++ static/js/logic4.js
@@ -36,7 +36,7 @@ var baseMaps = {
 // Create map object and set default layers
 var myMap = L.map("map", {
   center: [37.8044, -122.2712],
-  zoom: 11,
+  zoom: 12,
   layers: [street]
 });
 
@@ -99,4 +99,31 @@ d3.json(link).then(data => {
 
     }
   }).addTo(myMap);
+});
+
+// Grab the data with d3
+d3.json( "static/data/oakland_crime_data.json").then( function( response) {
+
+  // Create a new marker cluster group
+  var markers = L.markerClusterGroup();
+
+  // Loop through data
+  for ( var i = 0; i < response.length; i++) {
+
+    // // Set the data location property to a variable
+    // var location = response[i].address;
+
+    // Check for location property
+    if ( response[i].lng) {
+
+      // Add a new marker to the cluster group and bind a pop-up
+      markers.addLayer( L.marker( [lng, lat])
+        .bindPopup( response[i].crimetype));
+    }
+
+  }
+
+  // Add our marker cluster layer to the map
+  myMap.addLayer( markers);
+
 });
